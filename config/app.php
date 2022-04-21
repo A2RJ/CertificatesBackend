6bed460e49a522187b60c083c872ab8fcd700348
ARDIANSYAH PUTRA <xcz.ardiansyahputra2468@gmail.com>
2022-04-21 12:15:16 +0800

    Setting packages

--- config/app.php
+++ config/app.php
@@ -180,9 +180,10 @@ return [
         /**
          * thirtd package providers
          */
+        Barryvdh\DomPDF\ServiceProvider::class,
         Milon\Barcode\BarcodeServiceProvider::class,
-        SimpleSoftwareIO\QrCode\QrCodeServiceProvider::class
-
+        SimpleSoftwareIO\QrCode\QrCodeServiceProvider::class,
+        Novay\WordTemplate\WordTemplateServiceProvider::class,
     ],
 
     /*
@@ -201,9 +202,11 @@ return [
         /**
          * thirtd package aliases
          */
+        'PDF' => Barryvdh\DomPDF\Facade::class,
         'DNS1D' => Milon\Barcode\Facades\DNS1DFacade::class,
         'DNS2D' => Milon\Barcode\Facades\DNS2DFacade::class,
-        'QR' => SimpleSoftwareIO\QrCode\Facades\QrCode::class
+        'QR' => SimpleSoftwareIO\QrCode\Facades\QrCode::class,
+        'WordTemplate' => Novay\WordTemplate\Facade::class, 
     ])->toArray(),
 
 ];
