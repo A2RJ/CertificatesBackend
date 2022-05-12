6533a6933442de53fc3a54c34a4e4c505e6fd965
ARDIANSYAH PUTRA <xcz.ardiansyahputra2468@gmail.com>
2022-05-12 11:20:59 +0800

    Generate and push png barcode

--- resources/views/qr.blade.php
+++ resources/views/qr.blade.php
@@ -6,24 +6,44 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        #top {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: #f2f2f2;
+            opacity: 1;
+            z-index: 100;
+        }
+
+        #capture {
+            padding: 10px;
+            background: #f5da55;
+            width: 100%;
+            z-index: 10;
+        }
+    </style>
 </head>
 
 <body>
-    <div class="container mt-4">
-        <a href="">{{ $image }}</a>
+    <div id="top"></div>
+    <div id="capture" style="padding: 10px; background: #f5da55">
+        <h4 style="color: #000; ">Hello world!</h4>
     </div>
-
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="{{ asset('html2canvas/html2canvas.js') }}"></script>
     <script>
-        window.onload = function() {
-            var image = document.querySelector('a');
-            image.onclick = function() {
-                var win = window.open(this.href, '_blank');
-                win.focus();
-                return false;
-            }
-
-        }
-
+        html2canvas(document.getElementById("capture")).then(canvas => {
+            axios.post('/qrpost', {
+                dataURL: canvas.toDataURL("image/png")
+            }).then(res => {
+                console.log(res);
+            }).catch(err => {
+                console.log(err);
+            });
+        });
     </script>
 </body>
 
--- routes/web.php
+++ routes/web.php
@@ -25,8 +25,33 @@ Route::get('/', function () {
 });
 
 Route::get('/qr', function (Request $request) {
-    QrCode::generate('Make me into a QrCode!', public_path('qr_code.svg'));
-    echo '<img src="'.asset('qr_code.svg').'">';
+    // QrCode::generate('Make me into a QrCode!', public_path('qr_code.svg'));
+    // echo QrCode::generate('Make me into a QrCode!');
+    return view('qr');
+});
+
+Route::post('/qrpost', function (Request $request) {
+    // check if request has dataURL and is filetype image/png
+    if ($request->has('dataURL')) {
+        // get dataURL from request
+        $dataURL = $request->input('dataURL');
+        // get filetype from dataURL
+        $filetype = substr($dataURL, 5, strpos($dataURL, ';') - 5);
+        // get base64 encoded image from dataURL
+        $base64 = substr($dataURL, strpos($dataURL, ',') + 1);
+        // get image from base64 encoded image
+        $image = base64_decode($base64);
+        // get image filename
+        $filename = time() . '.' . $filetype;
+        // get image path
+        $path = public_path('qr_code.png');
+        // save image
+        file_put_contents($path, $image);
+        // return image path
+        return $path;
+    }
+    // return error message
+    return 'Error: invalid filetype or dataURL not found';
 });
 
 Route::get('/createCertificates', [ReplaceConvertController::class, 'createCertificates']);
